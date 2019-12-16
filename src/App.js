d0188f1070e8d5dbc927e05da2d5c682382ef1b5
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-12-17 01:39:00 +0700

    Fix some bugs.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import {
   Navigation,
   Home,
@@ -10,9 +10,9 @@ import {
   Feature,
   Contact,
   Register2,
-  Dasboard
-} from "./components";
-import "./App.css";
+  Dashboard
+} from './components'
+import './App.css'
 
 const App = () => {
   return (
@@ -20,37 +20,64 @@ const App = () => {
       <div>
         <Navigation />
         <Switch>
-          <Route path="/" exact={true}>
+          <Route
+            path="/"
+            exact={true}
+          >
             <Home />
           </Route>
-          <Route path="/about" exact={true}>
+          <Route
+            path="/about"
+            exact={true}
+          >
             <About />
           </Route>
-          <Route path="/log-in" exact={true}>
+          <Route
+            path="/log-in"
+            exact={true}
+          >
             <Login />
           </Route>
-          <Route path="/user/register">
+          <Route
+            path="/user/register"
+            exact={true}
+          >
             <Register2 />
           </Route>
-          <Route path="/company/register">
+          <Route
+            path="/company/register"
+            exact={true}
+          >
             <Register />
           </Route>
-          <Route path="/log-out" exact={true}>
+          <Route
+            path="/log-out"
+            exact={true}
+          >
             <Logout />
           </Route>
-          <Route path="/contacts" exact={true}>
+          <Route
+            path="/contacts"
+            exact={true}
+          >
             <Contact />
           </Route>
-          <Route path="/features" exact={true}>
+          <Route
+            path="/features"
+            exact={true}
+          >
             <Feature />
           </Route>
-          <Route path="/dasboard" exact={true}>
-            <Dasboard />
+          <Route
+            path="/dashboard"
+            exact={true}
+          >
+            <Dashboard />
           </Route>
         </Switch>
       </div>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,90 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import {
+  Accordion,
+  Card,
+  Col,
+  Row
+} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <p>ini dasboard</p>
-      </div>
-    );
+      <Row>
+        <Col md={{
+          span: 4
+        }}>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Accordion.Toggle
+                as={Card.Header}
+                eventKey="0"
+              >
+                Economy Reports
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <Link
+                    className="nav-link"
+                    to="/dashboard"
+                    style={{
+                      textDecoration: 'none'
+
+                    }}
+                  >
+                    Sales Reports
+                  </Link>
+                  <Link
+                    className="nav-link"
+                    to="/dashboard"
+                    style={{
+                      textDecoration: 'none'
+                    }}
+                  >
+                    Revenue Reports
+                  </Link>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle
+                as={Card.Header}
+                eventKey="1"
+              >
+                <Link
+                  className="nav-link"
+                  to="/dashboard"
+                  style={{
+                    textDecoration: 'none'
+                  }}
+                >
+                  Attendance Reports
+                </Link>
+              </Accordion.Toggle>
+            </Card>
+            <Card>
+              <Accordion.Toggle
+                as={Card.Header}
+                eventKey="2"
+              >
+                <Link
+                  className="nav-link"
+                  to="/dashboard"
+                  style={{
+                    textDecoration: 'none'
+                  }}
+                >
+                  Edit Profile
+                </Link>
+              </Accordion.Toggle>
+            </Card>
+          </Accordion>
+        </Col>
+        <Col md={{
+          span: 8
+        }}>
+        </Col>
+      </Row>
+    )
   }
 }
