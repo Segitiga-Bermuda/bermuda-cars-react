5fc1d5935b77f935805bfad4406c0583a7fa4fe7
GodsEnWrath <fastyazid@gmail.com>
2019-12-19 18:21:13 +0700

    Added changing year for line chart.

--- src/components/Line.js
+++ src/components/Line.js
@@ -26,6 +26,16 @@ const options = {
   datasetStroke: true,
   datasetStrokeWidth: 2,
   datasetFill: true,
+  scales: {
+    yAxes: [{
+      ticks: {
+        callback(value) {
+          // you can add your own method here (just an example)
+          return Number(value).toLocaleString('en')
+        }
+      }
+    }]
+  },
   legendTemplate:
     '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
 };
