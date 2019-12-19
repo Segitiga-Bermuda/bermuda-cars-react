0096c17ae0e9fbb5126485b1874e18835d022373
GodsEnWrath <fastyazid@gmail.com>
2019-12-19 19:41:51 +0700

    Add change date to pie chart.

--- src/components/Pie.js
+++ src/components/Pie.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { Pie } from "react-chartjs-2";
-import Carousel from "react-bootstrap/Carousel";
+import {
+  Form,
+  FormGroup,
+  FormText,
+  FormControl,
+  Button
+} from "react-bootstrap";
 import "../App.css";
 import { AXIOS } from "../helpers";
 
@@ -12,16 +18,28 @@ class chartPie extends Component {
       item: [],
       color: [],
       sold: [],
-      unsold: []
+      unsold: [],
+      year: 0,
+      maxYear: 0,
+      month: 'January'
     };
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleDateChange = this.handleDateChange.bind(this)
   }
 
-  componentDidMount() {
-    let year = new Date().getFullYear();
-    let month = new Date().getMonth();
+  handleChange(e) {
+      let val = e.target.value
+
+      this.setState({
+          [e.target.name]: val
+      })
+  }
+
+  handleDateChange() {
 
     AXIOS()
-      .get(`${API}/sales/sale/${year}/December`)
+      .get(`${API}/sales/sale/${this.state.year}/${this.state.month}`)
       .then(res => {
         let item = [];
         let color = [];
@@ -38,7 +56,38 @@ class chartPie extends Component {
           item: item,
           color: color,
           sold: sold,
-          unsold: unsold
+          unsold: unsold,
+          year: this.state.year,
+          month: this.state.month
+        });
+      });
+
+  }
+
+  componentDidMount() {
+    let year = new Date().getFullYear()
+
+    AXIOS()
+      .get(`${API}/sales/sale/${year}/${this.state.month}`)
+      .then(res => {
+        let item = [];
+        let color = [];
+        let sold = [];
+        let unsold = [];
+
+        res.data.data.forEach(element => {
+          item.push(element.item);
+          color.push(element.color);
+          sold.push(element.sold);
+          unsold.push(element.unsold);
+        });
+        this.setState({
+          item: item,
+          color: color,
+          sold: sold,
+          unsold: unsold,
+          year: year,
+          maxYear: year
         });
       });
   }
@@ -51,27 +100,83 @@ class chartPie extends Component {
           margin: "120px auto"
         }}
       >
-        <h3>2019</h3>
-        <Carousel controls={true} indicators={false}>
-          <Carousel.Item>
-            <h3>July </h3>
-          </Carousel.Item>
-          <Carousel.Item>
-            <h3>Agustus</h3>
-          </Carousel.Item>
-          <Carousel.Item>
-            <h3>September</h3>
-          </Carousel.Item>
-          <Carousel.Item>
-            <h3>Oktober</h3>
-          </Carousel.Item>
-          <Carousel.Item>
-            <h3>November</h3>
-          </Carousel.Item>
-          <Carousel.Item>
-            <h3>Desember</h3>
-          </Carousel.Item>
-        </Carousel>
+        <Form>
+            <FormGroup>
+              <FormText>
+                Year:
+              </FormText>
+              <FormControl
+                type="number"
+                step="1"
+                min="1970"
+                max={this.state.maxYear}
+                name="year"
+                id="year"
+                onChange={e => this.handleChange(e)}
+                value={this.state.year}
+              />
+            </FormGroup>
+                    <FormGroup>
+                        <FormText>
+                            Month:
+                        </FormText>
+                        <FormControl 
+                            as="select" 
+                            type="select" 
+                            onChange={e => this.handleChange(e)} 
+                            name="month" 
+                            id="month" 
+                            label="Month" 
+                            value={this.state.month}
+                        >
+                            <option value="January">
+                                January
+                            </option>
+                            <option value="February">
+                                February
+                            </option>
+                            <option value="March">
+                                March
+                           </option>
+                            <option value="April">
+                                April
+                            </option>
+                            <option value="May">
+                                May
+                            </option>
+                            <option value="June">
+                                June
+                            </option>
+                            <option value="July">
+                                July
+                           </option>
+                            <option value="August">
+                                August
+                            </option>
+                            <option value="September">
+                                September
+                            </option>
+                            <option value="October">
+                                October
+                            </option>
+                            <option value="November">
+                                November
+                           </option>
+                            <option value="December">
+                                December
+                            </option>
+                        </FormControl>
+                    </FormGroup>
+                <Button 
+                  onClick={e => this.handleDateChange(e)}
+                  style={{
+                    display: 'block',
+                    margin: '0 auto'
+                  }}
+                >
+                  Change Date
+                </Button>
+              </Form>
         <br />
         <Pie
           data={{
