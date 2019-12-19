1882db3d732e7616548681f515d546ad2b79b5e3
GodsEnWrath <fastyazid@gmail.com>
2019-12-19 18:07:57 +0700

    Added changing year for line chart.

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Line as LineChart } from "react-chartjs-2";
 import { AXIOS } from "../helpers";
+import {
+  Form,
+  FormText,
+  FormGroup,
+  FormControl,
+  Button
+} from 'react-bootstrap'
 
 let API = process.env.REACT_APP_API;
 
@@ -36,15 +43,20 @@ class chartLine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      year: 0,
+      maxYear: 0,
       chartData: {}
     };
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleYearChange = this.handleYearChange.bind(this)
   }
 
-  componentDidMount() {
-    let year = new Date().getFullYear();
-    let path = `${API}/sales/revenue/${year}`;
+  handleYearChange() {
+    let path = `${API}/sales/revenue/${this.state.year}`;
 
-    console.log(path);
+
+    console.log(path)
     AXIOS()
       .get(path)
       .then(res => {
@@ -172,7 +184,154 @@ class chartLine extends React.Component {
             ]
           }
         });
-        console.log(this.state.chartData);
+      });
+  }
+
+  handleChange(e) {
+      let val = e.target.value
+
+      this.setState({
+          [e.target.name]: val
+      })
+  }
+
+  componentDidMount() {
+    let year1 = new Date().getFullYear();
+
+    this.setState({
+      year: year1,
+      maxYear: year1
+    })
+
+    let path = `${API}/sales/revenue/${year1}`;
+
+    AXIOS()
+      .get(path)
+      .then(res => {
+        let totalProfit = [];
+        let totalLoss = [];
+        let month = [];
+        let newMonth = [];
+        let newProfit = [];
+        let newLoss = [];
+        let newMonth2 = [];
+        let newProfit2 = [];
+        let newLoss2 = [];
+
+        res.data.data.forEach(element => {
+          totalProfit.push(element.profitTotal);
+          totalLoss.push(element.totalLoss);
+          month.push(element.month);
+        });
+
+        for (let x = 0; x < month.length; ++x) {
+          switch (month[x]) {
+            case "January":
+              newMonth[0] = "January";
+              newProfit[0] = totalProfit[x];
+              newLoss[0] = totalLoss[x];
+              break;
+            case "February":
+              newMonth[1] = "February";
+              newProfit[1] = totalProfit[x];
+              newLoss[1] = totalLoss[x];
+              break;
+            case "March":
+              newMonth[2] = "March";
+              newProfit[2] = totalProfit[x];
+              newLoss[2] = totalLoss[x];
+              break;
+            case "April":
+              newMonth[3] = "April";
+              newProfit[3] = totalProfit[x];
+              newLoss[3] = totalLoss[x];
+              break;
+            case "May":
+              newMonth[4] = "May";
+              newProfit[4] = totalProfit[x];
+              newLoss[4] = totalLoss[x];
+              break;
+            case "June":
+              newMonth[5] = "June";
+              newProfit[5] = totalProfit[x];
+              newLoss[5] = totalLoss[x];
+              break;
+            case "July":
+              newMonth[6] = "July";
+              newProfit[6] = totalProfit[x];
+              newLoss[6] = totalLoss[x];
+              break;
+            case "August":
+              newMonth[7] = "August";
+              newProfit[7] = totalProfit[x];
+              newLoss[7] = totalLoss[x];
+              break;
+            case "September":
+              newMonth[8] = "September";
+              newProfit[8] = totalProfit[x];
+              newLoss[8] = totalLoss[x];
+              break;
+            case "October":
+              newMonth[9] = "October";
+              newProfit[9] = totalProfit[x];
+              newLoss[9] = totalLoss[x];
+              break;
+            case "November":
+              newMonth[10] = "November";
+              newProfit[10] = totalProfit[x];
+              newLoss[10] = totalLoss[x];
+              break;
+            case "December":
+              newMonth[11] = "December";
+              newProfit[11] = totalProfit[x];
+              newLoss[11] = totalLoss[x];
+              break;
+          }
+        }
+
+        for (let x = 0; x < newMonth.length; ++x) {
+          if (newMonth[x]) {
+            newMonth2.push(newMonth[x]);
+            newProfit2.push(newProfit[x]);
+            newLoss2.push(newLoss[x]);
+          }
+        }
+
+        this.setState({
+          chartData: {
+            labels: newMonth2,
+            datasets: [
+              {
+                label: "Profit",
+                fill: "false",
+                lineTension: 0.1,
+                borderCapStyle: "butt",
+                borderJoinStyle: "miter",
+                borderColor: "rgba(75,192,192,1)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: newProfit2
+              },
+              {
+                label: "Loss",
+                fill: "false",
+                lineTension: 0.1,
+                borderCapStyle: "butt",
+                borderJoinStyle: "miter",
+                borderColor: "rgba(240, 52, 52, 1)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: newLoss2
+              }
+            ]
+          }
+        });
       });
   }
   render() {
@@ -186,7 +345,32 @@ class chartLine extends React.Component {
         }}
       >
         <div style={styles.graphContainer}>
-          <h3>2019</h3>
+          <Form>
+                    <FormGroup>
+                        <FormText>
+                            Year:
+                        </FormText>
+                        <FormControl
+                            type="number"
+                            step="1"
+                            min="1970"
+                            max={this.state.maxYear}
+                            name="year"
+                            id="year"
+                            onChange={e => this.handleChange(e)}
+                            value={this.state.year}
+                        />
+                    </FormGroup>
+                    <Button 
+                      onClick={e => this.handleYearChange(e)}
+                      style={{
+                        display: 'block',
+                        margin: '0 auto'
+                      }}
+                    >
+                      Change Year
+                    </Button>
+                  </Form>
           <LineChart
             data={this.state.chartData}
             options={options}
